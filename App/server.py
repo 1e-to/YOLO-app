f96c2c5029507f5551aa3f5b71981e6551e664d8
Totmenina <elena.totmenina@intel.com>
2020-12-14 20:02:37 +0300

    Cleaning code

--- App/server.py
+++ App/server.py
@@ -1,13 +1,11 @@
-import random
 import socket, select
-from time import gmtime, strftime
-from random import randint
 import subprocess
 import sys
 
 ### NN ###
 import cv2
 import numpy as np
+
 
 net = cv2.dnn.readNet("C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\yolov3_coco\\yolov3.weights",
 					  "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\yolov3.cfg")
@@ -20,31 +18,15 @@ layer_names = net.getLayerNames()
 outputlayers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
 
-img_path = "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\image1.png"
+img_path = "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\image.png"
 res_path = "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\res.jpg"
 ##########
-
-imgcounter = 1
-basename = "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\image1.png"
-res_path = "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\res.jpg"
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-# 	s.bind((HOST, PORT))
-# 	s.listen()
-# 	conn, addr = s.accept()
-# 	with conn:
-# 		print('Connected by', addr)
-# 		while True:
-# 			data = conn.recv(1024)
-# 			if not data:
-# 				break
-# 			conn.sendall(data)
 
 connected_clients_sockets = []
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -68,36 +50,22 @@ while True:
 
 		else:
 			try:
-				# print("1")
 				data = sock.recv(4096)
-				# txt = data.decode()
-				# txt = str(data)
-				# print("txt", txt)
-
 				if data:
 					try:
 						data = data.decode()
-						print(type(data))
-						print("2")
-						# if data.startswith(' SIZE'):
 						if 'SIZE' in data:
-							print("3")
 							tmp = data.split()
-							print("tmp", tmp)
 							size = int(tmp[1])
 							global_size = size
-
-							print('got size', size)
 
 							sock.sendall(b'GOT SIZE')
 
 						elif 'BYE' in data:
-							print("4")
 							sock.shutdown()
 
 					except:
-						print("5")
-						myfile = open(basename, 'wb')
+						myfile = open(img_path, 'wb')
 						myfile.write(data)
 
 						data = sock.recv(global_size)
@@ -109,16 +77,11 @@ while True:
 
 						sock.sendall(b'GOT IMAGE')
 
-
-
-
-
-
-
 						#########
 						# nn_script = "C:\\Users\\etotmeni\\OneDrive - Intel Corporation\\Desktop\\App\\nn.py"
 						# subprocess.check_call([sys.executable, nn_script])
 						#########
+
 						im = cv2.imread(img_path)
 						img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
 						img_orig = img.shape
@@ -127,9 +90,9 @@ while True:
 						img_orig = img.shape
 						height, width, channels = img.shape
 
-						blob = cv2.dnn.blobFromImage(img,  # 	input image (with 1-, 3- or 4-channels).
-													 0.00392,  # 	spatial size for output image
-													 (416,416),  # 	
+						blob = cv2.dnn.blobFromImage(img, # input image (with 1-, 3- or 4-channels).
+													 0.00392, # spatial size for output image
+													 (416,416),
 													 (0,0,0),
 													 True,
 													 crop=False )
@@ -138,17 +101,17 @@ while True:
 						outs = net.forward(outputlayers)
 
 						# Showing info on the screen
-						conf_min = 0.5 # порог уверенности
+						conf_min = 0.5
 						class_ids = []
 						confidences = []
-						boxes = []  # [[43, 167, 381, 181]] - список координат найденных объектов
+						boxes = []  # [[43, 167, 381, 181]] - List of coordinates with detected objects
 						for out in outs:
 							for detection in out:
 								scores = detection[5:]
 								class_id = np.argmax(scores)
 								confidence = scores[class_id]
 								if confidence > conf_min:
-								# Object detected
+									# Object detected
 									center_x = int(detection[0] * width)
 									center_y = int(detection[1] * height)
 									w = int(detection[2] * width)
@@ -165,14 +128,13 @@ while True:
 						number_objects_detected = len(boxes)
 
 						if number_objects_detected:
-							classes = {0:'???', 13:'unknow', 56:'chair', 57:'sofa', 59:'bed', 60:'diningtable', 69:'&&&'}
+							classes = {0:'unknow', 56:'chair', 57:'sofa', 59:'bed', 60:'diningtable'}
 							for i in range(100):
 								if i not in classes.keys():
 									classes[i] = "???"
 
 							indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-							# периодически выскакивает keyError, но label присваивается верно
 							font = cv2.FONT_HERSHEY_PLAIN
 							color = 0
 							label = ""
@@ -180,32 +142,27 @@ while True:
 							y = 0
 							w = 0
 							h = 0
-							# for i in range(len(boxes)):
-							# Нужно предать:
+
 							x, y, w, h = boxes[0]
 							label = str(classes[class_ids[0]])
 							color = colors[0]
 
-							# # далее уже отрисовка
+							# Draw boxes of detected objects
 							cv2.rectangle(img, (x,y), (w,h), color, 1)
 							cv2.circle(img, (center_x, center_y), 10, (0,255,0))
 							cv2.putText(img, label, (x,y - 10), font, 1, (0,255,0), 2)
-							# plt.imshow(img)
+
 							img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 							res = cv2.imwrite(res_path ,img)
 							cv2.waitKey(0)
 							cv2.destroyAllWindows()
 
 						else:
-							print("Nothing in img")
 							img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 							res = cv2.imwrite(res_path ,img)
 
-
-
-
-
 						#########
+
 						sendfile = open(res_path, 'rb')
 						# sendfile = open(img_path, 'rb')
 						bytes = sendfile.read()
@@ -215,24 +172,16 @@ while True:
 
 						answer = sock.recv(4096)
 						answer = answer.decode()
-						print("answer", answer)
 
 						if answer == 'GOT SIZE':
 							sock.sendall(bytes)
 							answer = sock.recv(4096)
-							print('answer = %s' % answer)
 
 							if answer.decode() == 'GOT IMAGE' :
 								print('Image successfully send to client')
 
-						# sock.shutdown()
 			except Exception as e:
-				print("6")
 				print(e)
-				# sock.close()
-				# connected_clients_sockets.remove(sock)
 				continue
-		# print("7")
-		# imgcounter += 1
+
 server_socket.close()
-print("8")
